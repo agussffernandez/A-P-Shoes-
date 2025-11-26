73d74b9bc7da7c8ce29a0ab096d5f1e07f8de091
Agustina Belén Fernández <agustinabelenfernandez05@gmail.com>
2025-11-26 19:33:12 -0300

    configurando environments

---
+++ backend/src/api/config/environments.js
@@ -0,0 +1,16 @@
+import dotenv from "dotenv";
+
+// El método config carga las variables de entorno desde nuestro archivo .env
+dotenv.config();
+
+// Exportamos la información sensible
+export default {
+    // Si existe la variable PORT en .env, usala. Sino usa 3100 como puerto por defecto
+    port: process.env.PORT || 3100,
+    database: {
+        host: process.env.DB_HOST,
+        name: process.env.DB_NAME,
+        user: process.env.DB_USER,
+        password:  process.env.DB_PASSWORD
+    }
+}
